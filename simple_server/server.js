5c3a1d3199f3bb31d7af5e02371543fd787e0b99
Daniel-Xu <xwd.forever@gmail.com>
2013-12-03 20:36:46 -0500

    now we have module, the most important thing is exports, like erlang

--- simple_server/server.js
+++ simple_server/server.js
@@ -1,9 +1,15 @@
 var http = require("http");
 
-http.createServer(function(request, response){
-  console.log("request received");
-  response.writeHead(200, {"Content-Type": "text/plain"}) ;
-  response.write("hello");
-  response.end();
-}).listen(9090);
-console.log("server started");
+var start = function(){
+  http.createServer(function(request, response){
+    console.log("request received");
+    response.writeHead(200, {"Content-Type": "text/plain"}) ;
+    response.write("hello");
+    response.end();
+  }).listen(9090);
+  console.log("server started");
+}
+
+//module
+//exports 
+exports.start = start;
