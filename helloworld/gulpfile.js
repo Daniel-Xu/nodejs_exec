29e6f8e6e0d53efa986934685a5a7d923c6a7316
Daniel-Xu <xwd.forever@gmail.com>
2013-12-29 16:47:49 -0500

    gulp is more intuitive than grunt. It's more easy to config

---
+++ helloworld/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp')
+var uglify = require('gulp-uglify')
+
+gulp.task("compress", function(){
+    gulp.src('src/*.js').pipe(uglify()).pipe(gulp.dest('dst'))
+})
+
+gulp.task('default', function(){
+    gulp.run('compress')
+
+    gulp.watch('src/*.js', function(e){
+        gulp.run('compress')
+    })
+})
