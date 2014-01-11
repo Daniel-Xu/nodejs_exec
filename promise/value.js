edaf2c0e56c3e602ba0b0e5e502be10296ec33d0
Daniel-Xu <xwd.forever@gmail.com>
2014-01-11 10:33:44 -0500

    promise.then can be chained to do many things

---
+++ promise/value.js
@@ -0,0 +1,15 @@
+//construct a promise
+var q = require('q')
+var defer = q.defer()
+
+
+//
+function attachTitle(){
+    var a = Array.prototype.slice.call(arguments)
+    a.unshift('DR. ')
+    return a.join('')
+}
+
+
+defer.promise.then(attachTitle).then(console.log)
+defer.resolve('MANHATTAN')
