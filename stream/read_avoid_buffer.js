6a1aa949ee6c5f32a8ac959069ee99a6823c470f
Daniel-Xu <xwd.forever@gmail.com>
2014-02-28 01:02:46 -0500

    when we use
    node read_avoid_buffer.js | head -c5
    
    we only request 5 data, and when we stop requesting, there will be a
    SIGPIPE signal, and throw an error to process.stdout
    
    we then catch the error events and emit process exit events
    
    the exit events will print the times of our read call

--- stream/read_avoid_buffer.js
+++ stream/read_avoid_buffer.js
@@ -1,8 +1,24 @@
 var Readable = require("stream").Readable;
 var rs = new  Readable;
 
-var c = 97;
+var c = 97-1;
+//var c = 97;
 
+
+rs._read = function(){
+    if(c > 'z'.charCodeAt(0)){
+        //rs.push("\n");
+        return rs.push (null);
+    }
+
+    setTimeout(function(){
+        rs.push(String.fromCharCode(++c));
+    }, 100);
+}
+
+
+
+/*
 rs._read = function(){
     rs.push(String.fromCharCode(c++));
     if(c> 'z'.charCodeAt(0)){
@@ -11,7 +27,15 @@ rs._read = function(){
     
     }
 }
+*/
+
+
+process.on("exit", function(){
+    console.error("\n_read() called " + (c-97) + " times" );
+})
 
 rs.pipe(process.stdout)
+process.stdout.on("error", process.exit)
+
 
 
